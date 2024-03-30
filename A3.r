e1325c83d9b15b94f4463826fdb8bf22ea32d655
Shraddha Powar <scpowar@gmail.com>
2024-03-30 03:16:07 +0000

    changed A3

--- A3.r
+++ A3.r
@@ -2,3 +2,6 @@
 # Data Set: Kiva Crowd funding 
 # Data Set Link: https://www.kaggle.com/datasets/kiva/data-science-for-good-kiva-crowdfunding
 
+library(tidyverse)
+install.packages("leaflet")
+library(leaflet)
