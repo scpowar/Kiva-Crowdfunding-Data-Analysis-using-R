c0b1d2aeab5b56f6b605f6eeeb684ab2291ede7b
Shraddha Powar <scpowar@gmail.com>
2024-03-30 04:00:39 +0000

    reading data files

--- A3.r
+++ A3.r
@@ -10,8 +10,12 @@ library(leaflet)
 #Entry 1: Creating a dynamic path to read all the data files
 #Solution: https://stackoverflow.com/questions/34226187/r-dynamic-reference-to-files-for-read-csv
 
-folder_name <- 'NUIG BA/MS5130_Applied Analytics_R/R_A3/data/'
+folder_name <- 'D:/NUIG BA/MS5130_Applied Analytics_R/R_A3/data/'
 
 # Read data
-loans <- read_csv(file = paste0('D:/', folder_name,'kiva_loans.csv'))
+loans <- read_csv(file = paste0(folder_name,"kiva_loans.csv"))
+regions <- read_csv(file = paste0(folder_name,"kiva_mpi_region_locations.csv"))
+themes <- read_csv(file = paste0(folder_name,"loan_theme_ids.csv"))
+themes_region <- read_csv(file = paste0(folder_name,"loan_themes_by_region.csv"))
+
 head(loans)
