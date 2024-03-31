4b767e2ceece5028bdaacd47496fccb5d1c091be
Shraddha Powar <scpowar@gmail.com>
2024-03-31 06:51:40 +0100

    merged data files

--- A3.r
+++ A3.r
@@ -2,12 +2,12 @@
 # Data Set: Kiva Crowd funding 
 # Data Set Link: https://www.kaggle.com/datasets/kiva/data-science-for-good-kiva-crowdfunding
 
-# Required Libraries
+# install.packages("leaflet")
+# install.packages("DT")
 
+# Required Libraries
 library(tidyverse)
-#install.packages("leaflet")
 library(leaflet)
-#install.packages("DT")
 library(DT)
 
 ##Entry 1: Creating a dynamic path to read all the data files
@@ -16,36 +16,58 @@ library(DT)
 folder_name <- 'D:/NUIG BA/MS5130_Applied Analytics_R/R_A3/data/'
 
 # Load data
-
-loans <- read_csv(file = paste0(folder_name,"kiva_loans.csv"))
-regions <- read_csv(file = paste0(folder_name,"kiva_mpi_region_locations.csv"))
-themes <- read_csv(file = paste0(folder_name,"loan_theme_ids.csv"))
-themes_region <- read_csv(file = paste0(folder_name,"loan_themes_by_region.csv"))
+kiva_loans <- read_csv(file = paste0(folder_name,"kiva_loans.csv"))
+kiva_mpi_region_locations <- read_csv(file = paste0(folder_name,"kiva_mpi_region_locations.csv"))
+loan_themes <- read_csv(file = paste0(folder_name,"loan_theme_ids.csv"))
+loan_themes_by_region  <- read_csv(file = paste0(folder_name,"loan_themes_by_region.csv"))
 
 # Data Exploration
+str(kiva_loans)
+str(kiva_mpi_region_locations)
+str(loan_themes)
+str(loan_themes_by_region)
 
-# summary(loans)
-# head(loans)
-# str(loans)
+# Handling missing data
+kiva_loans <- kiva_loans[complete.cases(kiva_loans),]
+kiva_mpi_region_locations <- kiva_mpi_region_locations[complete.cases(kiva_mpi_region_locations),]
+loan_themes <- loan_themes[complete.cases(loan_themes),]
+loan_themes_by_region <- loan_themes_by_region[complete.cases(loan_themes_by_region),]
 
-glimpse(loans)
-glimpse(regions)
-glimpse(themes)
-glimpse(themes_region)
+# Shape of data
+dim(kiva_loans)
+dim(kiva_mpi_region_locations)
+dim(loan_themes)
+dim(loan_themes_by_region)
+
+# Merge Data
+merged_data <- kiva_loans %>%
+  left_join(kiva_mpi_region_locations, by = c("country" = "country", "region" = "region"))
+loan_themes_merged <- loan_themes %>% 
+  left_join(loan_themes_by_region, by = c("Loan Theme ID","Partner ID"))
+merged_data <- merged_data %>% 
+  left_join(loan_themes_merged, by = "id")
+
+# Explore merged data
+str(merged_data)
+
+# Shape of merged data
+dim(merged_data)
+
+str(merged_data)
 
 # Most Popular Sectors
 
-loans %>%
-  group_by(sector) %>%
+merged_data %>%
+  group_by(sector.x) %>%
   summarise(Count = n()) %>%
   arrange(desc(Count)) %>%
   ungroup() %>%
-  mutate(sector = reorder(sector,Count)) %>%
+  mutate(sector.x = reorder(sector.x,Count)) %>%
   head(10) %>%
   
-  ggplot(aes(x = sector,y = Count)) +
+  ggplot(aes(x = sector.x,y = Count)) +
   geom_bar(stat='identity',colour="white", fill = "#F1C40F") +
-  geom_text(aes(x = sector, y = 1, label = paste0("(",Count,")",sep="")),
+  geom_text(aes(x = sector.x, y = 1, label = paste0("(",Count,")",sep="")),
             hjust=0, vjust=.5, size = 4, colour = 'black',
             fontface = 'bold') +
   labs(x = 'Sector', 
